d80483c4519a172c2d5bc067fbd2d21af390aecb
Chetan <jellybeans33124@gmail.com>
2024-12-12 12:53:39 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
@@ -29,10 +30,36 @@ repl:
 			if valid[args[1]] {
 				fmt.Printf("%s is a shell builtin\n", args[1])
 			} else {
-				fmt.Printf("%s: not found\n", args[1])
+				paths := strings.Split(os.Getenv("PATH"), ":")
+				exists := false
+				var err error
+				for _, path := range paths {
+					exists, err = findFile(path, args[1])
+					if err != nil {
+						log.Fatal(err)
+					}
+					if exists {
+						fmt.Printf("%s is %s/%s\n", args[1], path, args[1])
+						break
+					}
+				}
+				if !exists {
+					fmt.Printf("%s: not found\n", args[1])
+				}
+
 			}
 		default:
 			fmt.Printf("%s: command not found\n", input)
 		}
 	}
+}
+
+func findFile(dir, fileName string) (bool, error) {
+	entries, _ := os.ReadDir(dir)
+	for _, entry := range entries {
+		if !entry.IsDir() && entry.Name() == fileName {
+			return true, nil
+		}
+	}
+	return false, nil
 }
